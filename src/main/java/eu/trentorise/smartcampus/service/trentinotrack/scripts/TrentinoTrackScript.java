700386bc4e31ef6732a38f830a73c37dc1b91098
gioadami <giordano.adami@gmail.com>
2013-11-20 15:09:21 +0100

    Walk tracks.

--- src/main/java/eu/trentorise/smartcampus/service/trentinotrack/scripts/TrentinoTrackScript.java
+++ src/main/java/eu/trentorise/smartcampus/service/trentinotrack/scripts/TrentinoTrackScript.java
@@ -2,11 +2,21 @@ package eu.trentorise.smartcampus.service.trentinotrack.scripts;
 
 import it.sayservice.platform.core.bus.common.exception.DataFlowException;
 
+import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import javax.xml.stream.XMLStreamException;
 
@@ -15,6 +25,7 @@ import org.apache.commons.lang.WordUtils;
 import com.google.protobuf.Message;
 
 import eu.trentorise.smartcampus.service.trentinotrack.data.message.Trentinotrack.BikeTrack;
+import eu.trentorise.smartcampus.service.trentinotrack.data.message.Trentinotrack.CampiglioDataPage;
 import eu.trentorise.smartcampus.service.trentinotrack.data.message.Trentinotrack.OpenDataResourcePage;
 import eu.trentorise.smartcampus.service.trentinotrack.data.message.Trentinotrack.OpenDataResourcePages;
 import eu.trentorise.smartcampus.service.trentinotrack.data.message.Trentinotrack.ResourceDescr;
@@ -27,6 +38,62 @@ public class TrentinoTrackScript {
 
 	private static final int MIN_LENGTH = 5000;
 
+	public List<Message> copy(Message msg) {
+		List<Message> result = new ArrayList<Message>();
+//		CampiglioDataPage.Builder builder = CampiglioDataPage.newBuilder((CampiglioDataPage)msg);
+//		builder.setTitle("???");
+		result.add(msg);
+		return result;
+	}
+	
+	public List<Message> downloadZip(ResourceDescr res, CampiglioDataPage page) throws MalformedURLException, IOException, DataFlowException {
+		ZipFile zf = downloadZip(res.getLink());
+		List<Message> result = new ArrayList<Message>();
+		
+		Enumeration<ZipEntry> entries = (Enumeration<ZipEntry>) zf.entries();
+		while (entries.hasMoreElements()) {
+			ZipEntry entry = entries.nextElement();
+			
+			BufferedInputStream in = new BufferedInputStream(zf.getInputStream(entry));
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+	    byte data[] = new byte[1024];
+	    int count;
+	    while((count = in.read(data,0,1024)) != -1)
+	    {
+	    		bos.write(data, 0, count);
+	    }		
+	    bos.close();
+	    
+	    BikeTrack bt = parseGpx(res, bos.toString());
+	    BikeTrack.Builder builder = BikeTrack.newBuilder(bt);
+	    builder.setLabel(page.getTitle() + " - " + bt.getLabel());
+			
+			result.add(builder.build());
+		}		
+		return result;
+	}	
+	
+	private ZipFile downloadZip(String address) throws MalformedURLException, IOException {
+		String tmp = System.getProperty("java.io.tmpdir");
+		File f = new File(tmp, "tmpzip.zip");
+		
+		FileOutputStream fos = new FileOutputStream(f);
+		
+		BufferedInputStream in = new BufferedInputStream(new URL(address).openStream());
+	    byte data[] = new byte[1024];
+	    int count;
+	    while((count = in.read(data,0,1024)) != -1)
+	    {
+	        fos.write(data, 0, count);
+	    }		
+	    fos.close();
+		
+			ZipFile zf = new ZipFile(f);
+			
+			return zf;
+	}
+	
 	public String getResourceRDFURL(OpenDataResourcePages pages) {
 		for (OpenDataResourcePage page : pages.getPagesList()) {
 			if ("rdf".equals(page.getFormat())) return page.getLink();
